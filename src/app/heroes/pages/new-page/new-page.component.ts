a8eda3bf0a35df1b16ba819118bdf3af7e81b778
andres.mera <feli041089@gmail.com>
2024-02-27 09:46:26 -0500

    dev : Establecer el valor del formulario.

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styles: ``
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
 
   public heroForm = new FormGroup({
     id: new FormControl<string>(''),
@@ -26,8 +29,26 @@ export class NewPageComponent {
   ];
 
   constructor(
-    private heroesService: HeroesService
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
   ) { }
+
+  ngOnInit(): void {
+    if (!this.router.url.includes('edit')) return;
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.heroesService.getHeroById(id)),
+      ).subscribe(hero => {
+        if (!hero) {
+          return this.router.navigateByUrl('/');
+        }
+
+        this.heroForm.reset(hero);
+        return;
+      });
+  }
 
   get currentHero(): Hero {
     const hero = this.heroForm.value as Hero;
