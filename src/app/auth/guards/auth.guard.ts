fde96900dd2a04c3fb7d21bee71376d84381ea91
andres.mera <feli041089@gmail.com>
2024-03-06 13:46:46 -0500

    dev : Guards de Angular.

---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanMatch, Route, UrlSegment, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanMatch, CanActivate {
+
+  constructor() { }
+
+  canMatch(route: Route, segments: UrlSegment[]): boolean | Observable<boolean> {
+    console.log('Can Match');
+    console.log({ route, segments });
+    throw new Error('Method not implemented.');
+  }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
+    console.log('Can Activate');
+    console.log({ route, state });
+    throw new Error('Method not implemented.');
+  }
+
+}
